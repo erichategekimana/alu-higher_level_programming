ed0b9ec81bb9891b3ee30a83896b3de429e92fa3
erichategekimana <h.eric@alustudent.com>
2025-06-26 08:22:43 -0700

    print numbers

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    if i < 10:
-        print("{}{}".format(0, (i)), end=', ')
+    if i < 99:
+        print("{:02d}".format(i), end=', ')
     else:
-        print(i, end=', ')
+        print("{}".format(i))
